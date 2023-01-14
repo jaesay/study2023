c5b362851799557f49fe6b0de2df44b72572f5cf
Kim Jae Seong <jaesay@ajou.ac.kr>
2023-01-14 21:22:59 +0900

    spring-redis-cluster-example 프로젝트 생성

---
+++ playground/spring-redis-cluster-example/src/main/java/com/example/springredisclusterexample/SpringRedisClusterExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springredisclusterexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisClusterExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringRedisClusterExampleApplication.class, args);
+  }
+
+}
---
+++ playground/spring-redis-cluster-example/src/test/java/com/example/springredisclusterexample/SpringRedisClusterExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springredisclusterexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisClusterExampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
