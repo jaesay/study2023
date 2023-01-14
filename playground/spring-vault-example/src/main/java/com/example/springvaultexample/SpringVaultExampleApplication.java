578900f4e686cdb89dd91924103d4ae4dae36a3e
Kim Jae Seong <jaesay@ajou.ac.kr>
2023-01-14 15:58:18 +0900

    spring-vault-example 프로젝트 생성

---
+++ playground/spring-vault-example/src/main/java/com/example/springvaultexample/SpringVaultExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springvaultexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVaultExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringVaultExampleApplication.class, args);
+  }
+
+}
---
+++ playground/spring-vault-example/src/test/java/com/example/springvaultexample/SpringVaultExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springvaultexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVaultExampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
