9838f213e64c0b95f9c539253a33a54b7727fd1a
Kim Jae Seong <jaesay@ajou.ac.kr>
2023-01-16 20:46:04 +0900

    spring-cloud-vault-example 프로젝트 생성

---
+++ playground/spring-cloud-vault-example/src/main/java/com/example/springcloudvaultexample/SpringCloudVaultExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudvaultexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudVaultExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringCloudVaultExampleApplication.class, args);
+  }
+
+}
