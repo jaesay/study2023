98ebaf206035173583503d5dc675f4534d5484bb
Kim Jae Seong <jaesay@ajou.ac.kr>
2023-01-22 03:38:19 +0900

    스프링 유레카에 서비스 등록

--- eureka-server/eureka-server/src/test/java/com/optimagrowth/eureka/EurekaServerApplicationTests.java
+++ eureka-server/eureka-server/src/test/java/com/optimagrowth/eureka/EurekaServerApplicationTests.java
@@ -1,9 +1,11 @@
 package com.optimagrowth.eureka;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class EurekaServerApplicationTests {
 
   @Test
