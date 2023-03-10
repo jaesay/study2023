eba35ffa94178f6dc9f287c076eb2d66800829d1
Kim Jae Seong <jaesay@ajou.ac.kr>
2023-03-10 22:21:07 +0900

    서블릿 요청 처리

--- helloboot/src/main/java/com/tobyspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/com/tobyspring/helloboot/HellobootApplication.java
@@ -8,6 +8,9 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 public class HellobootApplication {
 
@@ -19,9 +22,11 @@ public class HellobootApplication {
         protected void service(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
 
-          resp.setStatus(200);
-          resp.setHeader("Content-Type", "text/plain");
-          resp.getWriter().println("Hello Servlet");
+          String name = req.getParameter("name");
+
+          resp.setStatus(HttpStatus.OK.value());
+          resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+          resp.getWriter().println("Hello " + name);
         }
       }).addMapping("/hello");
     });
