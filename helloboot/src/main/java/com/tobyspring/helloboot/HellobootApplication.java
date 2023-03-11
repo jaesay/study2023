152c6221cb4272f42f9f35d8a4e49788d55f94c8
Kim Jae Seong <jaesay@ajou.ac.kr>
2023-03-11 14:41:03 +0900

    DispatcherServlet으로 전환

--- helloboot/src/main/java/com/tobyspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/com/tobyspring/helloboot/HellobootApplication.java
@@ -1,23 +1,16 @@
 package com.tobyspring.helloboot;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class HellobootApplication {
 
   public static void main(String[] args) {
     /* spring container 생성 */
-    GenericApplicationContext applicationContext = new GenericApplicationContext();
+    GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
     /* 구성정보 */
     applicationContext.registerBean(HelloController.class);
     applicationContext.registerBean(SimpleHelloService.class);
@@ -28,26 +21,8 @@ public class HellobootApplication {
 
     /* servlet container 생성 */
     WebServer webServer = serverFactory.getWebServer(servletContext -> {
-      servletContext.addServlet("frontcontroller", new HttpServlet() {
-        @Override
-        protected void service(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-
-          // TODO: 인증, 보안, 다국어, 공통 기능
-          if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-            String name = req.getParameter("name");
-
-            HelloController helloController = applicationContext.getBean(HelloController.class);
-            String result = helloController.hello(name);
-
-            resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-            resp.getWriter().println(result);
-
-          } else {
-            resp.setStatus(HttpStatus.NOT_FOUND.value());
-          }
-        }
-      }).addMapping("/*");
+      servletContext.addServlet("dispatcherServlet", new DispatcherServlet(applicationContext))
+          .addMapping("/*");
     });
     webServer.start();
   }
