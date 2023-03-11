08cbbbcae949b9dd170de8c6d9a34d26d6e7d0ee
Kim Jae Seong <jaesay@ajou.ac.kr>
2023-03-11 23:53:17 +0900

    테스트 코드를 이용한 테스트

---
+++ helloboot/src/test/java/com/tobyspring/helloboot/HelloApiTest.java
@@ -0,0 +1,25 @@
+package com.tobyspring.helloboot;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+class HelloApiTest {
+
+  @Test
+  void helloApi() {
+    TestRestTemplate restTemplate = new TestRestTemplate();
+
+    ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8080/hello?name={name}",
+        String.class, "Spring");
+
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    assertThat(response.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
+    assertThat(response.getBody()).isEqualTo("Hello Spring");
+  }
+}
