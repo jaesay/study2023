70cf439a41461357d9a9a109c25c26884b22918a
Kim Jae Seong <jaesay@ajou.ac.kr>
2023-03-12 16:15:56 +0900

    자동 구성 애노테이션 적용

--- helloboot/src/main/java/com/tobyspring/config/autoconfig/DispatcherServletConfig.java
+++ helloboot/src/main/java/com/tobyspring/config/autoconfig/DispatcherServletConfig.java
@@ -1,10 +1,10 @@
 package com.tobyspring.config.autoconfig;
 
+import com.tobyspring.config.MyAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
+@MyAutoConfiguration
 public class DispatcherServletConfig {
 
   @Bean
