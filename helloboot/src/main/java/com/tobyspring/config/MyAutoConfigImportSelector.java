396349b9cc396f21f527967910d7e1703bbb3e06
Kim Jae Seong <jaesay@ajou.ac.kr>
2023-03-12 16:08:46 +0900

    자동 구성 정보 파일 분리

--- helloboot/src/main/java/com/tobyspring/config/MyAutoConfigImportSelector.java
+++ helloboot/src/main/java/com/tobyspring/config/MyAutoConfigImportSelector.java
@@ -1,15 +1,25 @@
 package com.tobyspring.config;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
 
+  private final ClassLoader classLoader;
+
+  public MyAutoConfigImportSelector(ClassLoader classLoader) {
+    this.classLoader = classLoader;
+  }
+
   @Override
   public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-    return new String[] {
-      "com.tobyspring.config.autoconfig.DispatcherServletConfig",
-      "com.tobyspring.config.autoconfig.TomcatWebServerConfig",
-    };
+    List<String> autoConfigs = new ArrayList<>();
+
+    ImportCandidates.load(MyAutoConfiguration.class, classLoader).forEach(autoConfigs::add);
+
+    return autoConfigs.toArray(new String[0]);
   }
 }
