882115da656d698197d8e1e8fe5561ccf5079a74
Kim Jae Seong <jaesay@ajou.ac.kr>
2023-03-14 00:23:12 +0900

    자동 구성 정보 대체하기

--- helloboot/src/main/java/com/tobyspring/config/autoconfig/JettyWebServerConfig.java
+++ helloboot/src/main/java/com/tobyspring/config/autoconfig/JettyWebServerConfig.java
@@ -2,6 +2,7 @@ package com.tobyspring.config.autoconfig;
 
 import com.tobyspring.config.MyAutoConfiguration;
 import com.tobyspring.config.MyConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 public class JettyWebServerConfig {
 
   @Bean("JettyWebServerFactory")
+  @ConditionalOnMissingBean
   public ServletWebServerFactory servletWebServerFactory() {
     return new JettyServletWebServerFactory();
   }
