16e1903a028a2084f9b68cf8667c5ccb734f0583
Kim Jae Seong <jaesay@ajou.ac.kr>
2023-03-15 23:45:52 +0900

    JdbcTemplate과 트랜잭션 매니저 구성

--- helloboot/src/main/java/com/tobyspring/config/autoconfig/DataSourceConfig.java
+++ helloboot/src/main/java/com/tobyspring/config/autoconfig/DataSourceConfig.java
@@ -7,12 +7,17 @@ import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Driver;
 import javax.sql.DataSource;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.support.JdbcTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @MyAutoConfiguration
 @MyConditionalOnClass("org.springframework.jdbc.core.JdbcOperations")
 @EnableMyConfigurationProperties(MyDataSourceProperties.class)
+@EnableTransactionManagement
 public class DataSourceConfig {
 
   @Bean
@@ -41,5 +46,19 @@ public class DataSourceConfig {
     dataSource.setPassword(properties.getPassword());
 
     return dataSource;
+  }
+
+  @Bean
+  @ConditionalOnSingleCandidate(DataSource.class)
+  @ConditionalOnMissingBean
+  JdbcTemplate jdbcTemplate(DataSource dataSource) {
+    return new JdbcTemplate(dataSource);
+  }
+
+  @Bean
+  @ConditionalOnSingleCandidate(DataSource.class)
+  @ConditionalOnMissingBean
+  JdbcTransactionManager jdbcTransactionManager(DataSource dataSource) {
+    return new JdbcTransactionManager(dataSource);
   }
 }
---
+++ helloboot/src/test/java/com/tobyspring/helloboot/JdbcTemplateTest.java
@@ -0,0 +1,29 @@
+package com.tobyspring.helloboot;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@HellobootTest
+class JdbcTemplateTest {
+
+  @Autowired
+  JdbcTemplate jdbcTemplate;
+
+  @BeforeEach
+  void init() {
+    jdbcTemplate.execute("create table if not exists hello(name varchar(50) primary key, count int)");
+  }
+
+  @Test
+  void insertAndQuery() {
+    jdbcTemplate.update("insert into hello values(?, ?)", "Jaeseong", 3);
+    jdbcTemplate.update("insert into hello values(?, ?)", "Spring", 1);
+
+    Long count = jdbcTemplate.queryForObject("select count(*) from hello", Long.class);
+    assertThat(count).isEqualTo(2);
+  }
+}
