f70d3cfa94f857372b6ba261714c46f6db98b56f
Kim Jae Seong <jaesay@ajou.ac.kr>
2023-03-17 23:40:18 +0900

    자동 구성 조건 결과 확인

--- springboot-ac/src/main/java/com/example/springbootac/SpringbootAcApplication.java
+++ springboot-ac/src/main/java/com/example/springbootac/SpringbootAcApplication.java
@@ -1,10 +1,31 @@
 package com.example.springbootac;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionEvaluationReport;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootAcApplication {
+
+  @Bean
+  ApplicationRunner run(ConditionEvaluationReport report) {
+    /* 자동구성 클래스 Condition 결과 확인  */
+    return args -> {
+      System.out.println(report.getConditionAndOutcomesBySource().entrySet().stream()
+          .filter(co -> co.getValue().isFullMatch())
+          .filter(co -> !co.getKey().contains("Jmx"))
+          .map(co -> {
+            System.out.println(co.getKey());
+            co.getValue().forEach(v -> {
+              System.out.println("\t" + v.getOutcome());
+            });
+            System.out.println();
+            return co;
+          }).count());
+    };
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(SpringbootAcApplication.class, args);
