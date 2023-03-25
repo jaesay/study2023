8631f73a569eb4939d2072cfbac32415953b0923
Kim Jae Seong <jaesay@ajou.ac.kr>
2023-03-25 15:21:32 +0900

    주석 추가

--- querydsl/src/test/java/com/example/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/com/example/querydsl/QuerydslBasicTest.java
@@ -12,6 +12,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import javax.persistence.EntityManager;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,6 +24,11 @@ class QuerydslBasicTest {
 
   @Autowired
   EntityManager em;
+  /**
+   * JPAQueryFactory를 필드로 제공하면 동시성 문제는 어떻게 될까?
+   * 동시성 문제는 JPAQueryFactory를 생성할 때 제공하는 EntityManager(em)에 달려있다.
+   * 스프링 프레임워크는 여러 쓰레드에서 동시에 같은 EntityManager에 접근해도, 트랜잭션 마다 별도의 영속성 컨텍스트를 제공하기 때문에, 동시성 문제는 걱정하지 않아도 된다.
+   */
   JPAQueryFactory queryFactory;
 
   @BeforeEach
@@ -42,8 +48,15 @@ class QuerydslBasicTest {
     em.persist(member4);
   }
 
+  /**
+   * Querydsl은 JPQL 빌더
+   * JPQL: 문자(실행 시점 오류), Querydsl: 코드(컴파일 시점 오류)
+   * JPQL: 파라미터 바인딩 직접, Querydsl: 파라미터 바인딩 자동 처리
+   */
   @Test
+  @DisplayName("JPQL vs Querydsl")
   void startJPQL() {
+    /* JPQL */
     String qlString = "select m from Member m " +
         "where m.username = :username";
 
@@ -52,20 +65,22 @@ class QuerydslBasicTest {
         .getSingleResult();
 
     assertThat(findMember.getUsername()).isEqualTo("member1");
-  }
 
-  @Test
-  void startQuerydsl() {
-    Member findMember = queryFactory
+    /* Querydsl */
+    /* 같은 테이블을 조인해야 하는 경우가 아니면 기본 인스턴스를 사용하자. */
+//    QMember qMember = new QMember("m"); // 별칭 직접 지정
+//    QMember qMember = QMember.member; // 기본 인스턴스 사용
+    Member findMember2 = queryFactory
         .select(member)
         .from(member)
         .where(member.username.eq("member1"))
         .fetchOne();
 
-    assertThat(findMember.getUsername()).isEqualTo("member1");
+    assertThat(findMember2.getUsername()).isEqualTo("member1");
   }
 
   @Test
+  @DisplayName("검색 조건 쿼리")
   void search() {
     Member findMember = queryFactory
         .selectFrom(member)
@@ -74,38 +89,44 @@ class QuerydslBasicTest {
         .fetchOne();
 
     assertThat(findMember.getUsername()).isEqualTo("member1");
-  }
 
-  @Test
-  void searchAndParam() {
-    Member findMember = queryFactory
+    /* AND 조건을 가변인수로 처리 */
+    Member findMember2 = queryFactory
         .selectFrom(member)
         .where(member.username.eq("member1"),
             member.age.eq(10))
         .fetchOne();
 
-    assertThat(findMember.getUsername()).isEqualTo("member1");
+    assertThat(findMember2.getUsername()).isEqualTo("member1");
   }
 
   @Test
+  @DisplayName("결과 조회")
   void resultFetch() {
+    /* 리스트 조회, 데이터 없으면 빈 리스트 반환 */
     List<Member> fetch = queryFactory
         .selectFrom(member)
         .fetch();
 
+    /* fetchOne() : 단 건 조회
+    결과가 없으면 : null
+    결과가 둘 이상이면 : com.querydsl.core.NonUniqueResultException */
     Member findMember1 = queryFactory
         .selectFrom(member)
         .where(member.id.eq(1L))
         .fetchOne();
 
+    /* limit(1).fetchOne() */
     Member findMember2 = queryFactory
         .selectFrom(member)
         .fetchFirst();
 
+    /* 페이징 정보 포함, total count 쿼리 추가 실행 */
     QueryResults<Member> results = queryFactory
         .selectFrom(member)
         .fetchResults();
 
+    /* count 쿼리로 변경해서 count 수 조회 */
     long count = queryFactory
         .selectFrom(member)
         .fetchCount();
@@ -119,6 +140,7 @@ class QuerydslBasicTest {
    * 단 2에서 회원 이름이 없으면 마지막에 출력(nulls last)
    */
   @Test
+  @DisplayName("정렬")
   void sort() {
     em.persist(new Member(null, 100));
     em.persist(new Member("member5", 100));
@@ -138,6 +160,7 @@ class QuerydslBasicTest {
   }
 
   @Test
+  @DisplayName("페이징")
   void paging() {
     List<Member> result = queryFactory
         .selectFrom(member)
@@ -147,6 +170,12 @@ class QuerydslBasicTest {
         .fetch();
     assertThat(result.size()).isEqualTo(2);
 
+    /* 전체 조회 수가 필요하면? */
+    /* 주의: count 쿼리가 실행되니 성능상 주의! */
+    /* 참고: 실무에서 페이징 쿼리를 작성할 때, 데이터를 조회하는 쿼리는 여러 테이블을 조인해야 하지만,
+    count 쿼리는 조인이 필요 없는 경우도 있다.
+    그런데 이렇게 자동화된 count 쿼리는 원본 쿼리와 같이 모두 조인을 해버리기 때문에 성능이 안나올 수 있다.
+    count 쿼리에 조인이 필요없는 성능 최적화가 필요하다면, count 전용 쿼리를 별도로 작성해야 한다.*/
     QueryResults<Member> queryResults = queryFactory
         .selectFrom(member)
         .orderBy(member.username.desc())
@@ -161,6 +190,7 @@ class QuerydslBasicTest {
   }
 
   @Test
+  @DisplayName("집합")
   public void aggregation() {
     List<Tuple> result = queryFactory
         .select(member.count(),
