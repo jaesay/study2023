c3bdb6318efd3eacda2f1863acf4e1f1073d0903
Kim Jae Seong <jaesay@ajou.ac.kr>
2023-03-28 00:00:51 +0900

    스프링 데이터 페이징 활용1 - Querydsl 페이징 연동

--- querydsl/src/main/java/com/example/querydsl/repository/MemberRepositoryImpl.java
+++ querydsl/src/main/java/com/example/querydsl/repository/MemberRepositoryImpl.java
@@ -7,10 +7,14 @@ import static org.springframework.util.StringUtils.hasText;
 import com.example.querydsl.dto.MemberSearchCondition;
 import com.example.querydsl.dto.MemberTeamDto;
 import com.example.querydsl.dto.QMemberTeamDto;
+import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import javax.persistence.EntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
 /**
  * Spring Data의 기본 Repository 구현체 검색 전략 때문에 커스텀 인터페이스 이름에 "Impl" 접미사를 추가해야 한다.
@@ -41,6 +45,80 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
             ageGoe(condition.getAgeGoe()),
             ageLoe(condition.getAgeLoe()))
         .fetch();
+  }
+
+  /**
+   * 전체 카운트를 한번에 조회하는 단순한 방법, fetchResults() 사용
+   * Querydsl이 제공하는 fetchResults() 를 사용하면 내용과 전체 카운트를 한번에 조회할 수 있다.(실제 쿼리는 2번 호출)
+   * fetchResult() 는 카운트 쿼리 실행시 필요없는 order by 는 제거한다.
+   * @param condition
+   * @param pageable
+   * @return
+   */
+  @Override
+  public Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable) {
+    QueryResults<MemberTeamDto> results = queryFactory
+        .select(new QMemberTeamDto(
+            member.id,
+            member.username,
+            member.age,
+            team.id,
+            team.name))
+        .from(member)
+        .leftJoin(member.team, team)
+        .where(usernameEq(condition.getUsername()),
+            teamNameEq(condition.getTeamName()),
+            ageGoe(condition.getAgeGoe()),
+            ageLoe(condition.getAgeLoe()))
+        .offset(pageable.getOffset())
+        .limit(pageable.getPageSize())
+        .fetchResults();
+
+    List<MemberTeamDto> content = results.getResults();
+    long total = results.getTotal();
+
+    return new PageImpl<>(content, pageable, total);
+  }
+
+  /**
+   * 복잡한 페이징
+   * 데이터 조회 쿼리와, 전체 카운트 쿼리를 분리
+   * 전체 카운트를 조회 하는 방법을 최적화 할 수 있으면 이렇게 분리하면 된다. (예를 들어서 전체 카운트를 조회할 때 조인 쿼리를 줄일 수 있다면 상당한 효과가 있다.)
+   * 코드를 리펙토링해서 내용 쿼리과 전체 카운트 쿼리를 읽기 좋게 분리하면 좋다.
+   * @param condition
+   * @param pageable
+   * @return
+   */
+  @Override
+  public Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable) {
+    List<MemberTeamDto> content = queryFactory
+        .select(new QMemberTeamDto(
+            member.id,
+            member.username,
+            member.age,
+            team.id,
+            team.name))
+        .from(member)
+        .leftJoin(member.team, team)
+        .where(usernameEq(condition.getUsername()),
+            teamNameEq(condition.getTeamName()),
+            ageGoe(condition.getAgeGoe()),
+            ageLoe(condition.getAgeLoe()))
+        .offset(pageable.getOffset())
+        .limit(pageable.getPageSize())
+        .fetch();
+
+    long total = queryFactory
+        .select(member)
+        .from(member)
+        .leftJoin(member.team, team)
+        .where(usernameEq(condition.getUsername()),
+            teamNameEq(condition.getTeamName()),
+            ageGoe(condition.getAgeGoe()),
+            ageLoe(condition.getAgeLoe()))
+        .fetchCount();
+
+    return new PageImpl<>(content, pageable, total);
   }
 
   private BooleanExpression usernameEq(String username) {
