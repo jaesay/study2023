f537981e44802d271fbf6d1fb815292d578b4bf0
Kim Jae Seong <jaesay@ajou.ac.kr>
2023-03-28 00:09:22 +0900

    스프링 데이터 페이징 활용2 - CountQuery 최적화

--- querydsl/src/main/java/com/example/querydsl/repository/MemberRepositoryImpl.java
+++ querydsl/src/main/java/com/example/querydsl/repository/MemberRepositoryImpl.java
@@ -7,14 +7,17 @@ import static org.springframework.util.StringUtils.hasText;
 import com.example.querydsl.dto.MemberSearchCondition;
 import com.example.querydsl.dto.MemberTeamDto;
 import com.example.querydsl.dto.QMemberTeamDto;
+import com.example.querydsl.entity.Member;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import javax.persistence.EntityManager;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 
 /**
  * Spring Data의 기본 Repository 구현체 검색 전략 때문에 커스텀 인터페이스 이름에 "Impl" 접미사를 추가해야 한다.
@@ -108,17 +111,24 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
         .limit(pageable.getPageSize())
         .fetch();
 
-    long total = queryFactory
+    JPAQuery<Member> countQuery = queryFactory
         .select(member)
         .from(member)
         .leftJoin(member.team, team)
         .where(usernameEq(condition.getUsername()),
             teamNameEq(condition.getTeamName()),
             ageGoe(condition.getAgeGoe()),
-            ageLoe(condition.getAgeLoe()))
-        .fetchCount();
+            ageLoe(condition.getAgeLoe()));
+//        .fetchCount();
 
-    return new PageImpl<>(content, pageable, total);
+//    return new PageImpl<>(content, pageable, total);
+    /*
+    * 스프링 데이터 라이브러리가 제공
+    * count 쿼리가 생략 가능한 경우 생략해서 처리
+    * - 페이지 시작이면서 컨텐츠 사이즈가 페이지 사이즈보다 작을 때
+    * - 마지막 페이지 일 때 (offset + 컨텐츠 사이즈를 더해서 전체 사이즈 구함)
+    * */
+    return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchCount);
   }
 
   private BooleanExpression usernameEq(String username) {
