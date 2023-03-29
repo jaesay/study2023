58523a8fa1e9fbc421e5e29238ba9c328e900696
Kim Jae Seong <jaesay@ajou.ac.kr>
2023-03-29 20:44:48 +0900

    gs-spring-data-reactive-redis 프로젝트 생성

---
+++ playground/gs-spring-data-reactive-redis/src/test/java/springguides/gsspringdatareactiveredis/GsSpringDataReactiveRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package springguides.gsspringdatareactiveredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsSpringDataReactiveRedisApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
