1a2e949dd2ad68e365f51c49cbba00f4ec11018d
Kim Jae Seong <jaesay@ajou.ac.kr>
2023-04-12 23:18:26 +0900

    spring-lettucemod 프로젝트 생성

---
+++ spring-lettucemod/src/test/java/com/example/springlettucemod/SpringLettucemodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springlettucemod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLettucemodApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
