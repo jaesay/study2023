3e3f68b04bde625e536a26f05b2ca23847c0cdf5
Kim Jae Seong <jaesay@ajou.ac.kr>
2023-04-28 21:30:43 +0900

    toby-spring-rp 프로젝트 생성

---
+++ toby-spring-rp/src/main/java/com/example/tobyspringrp/TobySpringRpApplication.java
@@ -0,0 +1,13 @@
+package com.example.tobyspringrp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobySpringRpApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TobySpringRpApplication.class, args);
+  }
+
+}
---
+++ toby-spring-rp/src/test/java/com/example/tobyspringrp/TobySpringRpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tobyspringrp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobySpringRpApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
