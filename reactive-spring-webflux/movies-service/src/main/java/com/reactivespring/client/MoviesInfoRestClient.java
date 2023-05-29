3fbb2a0d16ea539c5fc6b83605580bccd492472a
Kim Jae Seong <jaesay@ajou.ac.kr>
2023-05-29 22:39:56 +0900

    Handling Network Exceptions in WebClient

--- reactive-spring-webflux/movies-service/src/main/java/com/reactivespring/client/MoviesInfoRestClient.java
+++ reactive-spring-webflux/movies-service/src/main/java/com/reactivespring/client/MoviesInfoRestClient.java
@@ -1,12 +1,17 @@
 package com.reactivespring.client;
 
 import com.reactivespring.domain.MovieInfo;
+import com.reactivespring.exception.MoviesInfoClientException;
+import com.reactivespring.exception.MoviesInfoServerException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @Component
+@Slf4j
 public class MoviesInfoRestClient {
 
   private final WebClient webClient;
@@ -23,6 +28,27 @@ public class MoviesInfoRestClient {
         .get()
         .uri(url, movieId)
         .retrieve()
+        .onStatus(HttpStatus::is4xxClientError, clientResponse -> {
+          log.info("Status code is : {}", clientResponse.statusCode().value());
+          if (clientResponse.statusCode() == HttpStatus.NOT_FOUND) {
+            return Mono.error(new MoviesInfoClientException(
+                "There is no MovieInfo available for the passed in id : " + movieId,
+                clientResponse.statusCode().value()
+            ));
+          }
+
+          return clientResponse.bodyToMono(String.class)
+              .flatMap(responseBody -> Mono.error(new MoviesInfoClientException(
+                  responseBody, clientResponse.statusCode().value()
+              )));
+        })
+        .onStatus(HttpStatus::is5xxServerError, clientResponse -> {
+          log.info("Status code is : {}", clientResponse.statusCode().value());
+
+          return clientResponse.bodyToMono(String.class)
+              .flatMap(responseBody -> Mono.error(new MoviesInfoServerException(
+                  "Server exception in MoviesInfoService : " + responseBody)));
+        })
         .bodyToMono(MovieInfo.class)
         .log();
   }
--- reactive-spring-webflux/movies-service/src/main/java/com/reactivespring/client/ReviewsRestClient.java
+++ reactive-spring-webflux/movies-service/src/main/java/com/reactivespring/client/ReviewsRestClient.java
@@ -1,13 +1,19 @@
 package com.reactivespring.client;
 
 import com.reactivespring.domain.Review;
+import com.reactivespring.exception.ReviewsClientException;
+import com.reactivespring.exception.ReviewsServerException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Component
+@Slf4j
 public class ReviewsRestClient {
 
   private final WebClient webClient;
@@ -27,6 +33,22 @@ public class ReviewsRestClient {
         .get()
         .uri(url)
         .retrieve()
+        .onStatus(HttpStatus::is4xxClientError, clientResponse -> {
+          log.info("Status code is : {}", clientResponse.statusCode().value());
+          if (clientResponse.statusCode() == HttpStatus.NOT_FOUND) {
+            return Mono.empty();
+          }
+
+          return clientResponse.bodyToMono(String.class)
+              .flatMap(responseBody -> Mono.error(new ReviewsClientException(responseBody)));
+        })
+        .onStatus(HttpStatus::is5xxServerError, clientResponse -> {
+          log.info("Status code is : {}", clientResponse.statusCode().value());
+
+          return clientResponse.bodyToMono(String.class)
+              .flatMap(responseBody -> Mono.error(new ReviewsServerException(
+                  "Server exception in ReviewsService : " + responseBody)));
+        })
         .bodyToFlux(Review.class)
         .log();
   }
