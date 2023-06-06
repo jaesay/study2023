95afcf393a1507044bbf64440eb786abc0cb4f33
Kim Jae Seong <jaesay@ajou.ac.kr>
2023-06-06 20:04:10 +0900

    redis-sorting 프로젝트 생성

---
+++ playground/redis-sorting/src/main/java/com/example/redissorting/RedisSortingApplication.java
@@ -0,0 +1,13 @@
+package com.example.redissorting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisSortingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RedisSortingApplication.class, args);
+  }
+
+}
