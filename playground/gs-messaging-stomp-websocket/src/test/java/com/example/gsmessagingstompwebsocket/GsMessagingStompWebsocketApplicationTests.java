1e2270718ef9a686eb3c80abc0a26c74bcd527af
Kim Jae Seong <jaesay@ajou.ac.kr>
2023-06-08 22:52:09 +0900

    gs-messaging-stomp-websocket 프로젝트 생성

---
+++ playground/gs-messaging-stomp-websocket/src/test/java/com/example/gsmessagingstompwebsocket/GsMessagingStompWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gsmessagingstompwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsMessagingStompWebsocketApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
