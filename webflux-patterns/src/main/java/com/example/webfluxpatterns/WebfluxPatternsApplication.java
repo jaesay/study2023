a5e8a1348b563655eda7dc4fb34052372ffe1693
Kim Jae Seong <jaesay@ajou.ac.kr>
2023-06-13 22:11:59 +0900

    webflux-patterns 프로젝트 생성

---
+++ webflux-patterns/src/main/java/com/example/webfluxpatterns/WebfluxPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.example.webfluxpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxPatternsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WebfluxPatternsApplication.class, args);
+  }
+
+}
