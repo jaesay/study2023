24e2577af5af4e5664ba726c63ea2bfe84964cd3
Kim Jae Seong <jaesay@ajou.ac.kr>
2023-06-24 23:29:08 +0900

    Reservation Service 추가

---
+++ webflux-patterns/src/main/java/com/example/webfluxpatterns/_05_splitter/dto/ReservationResponse.java
@@ -0,0 +1,19 @@
+package com.example.webfluxpatterns._05_splitter.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor(staticName = "create")
+public class ReservationResponse {
+
+  private UUID reservationId;
+  private Integer price;
+  private List<ReservationItemResponse> items;
+
+}
