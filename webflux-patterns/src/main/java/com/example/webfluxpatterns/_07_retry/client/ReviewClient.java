507514fa9e5a6e1b86743e2b0aee15f0ee99ae4a
Kim Jae Seong <jaesay@ajou.ac.kr>
2023-06-25 17:53:46 +0900

    Retry 설정 추가

--- webflux-patterns/src/main/java/com/example/webfluxpatterns/_07_retry/client/ReviewClient.java
+++ webflux-patterns/src/main/java/com/example/webfluxpatterns/_07_retry/client/ReviewClient.java
@@ -2,10 +2,13 @@ package com.example.webfluxpatterns._07_retry.client;
 
 import com.example.webfluxpatterns._07_retry.dto.Review;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
 
+import java.time.Duration;
 import java.util.Collections;
 import java.util.List;
 
@@ -25,9 +28,13 @@ public class ReviewClient {
         .get()
         .uri("{id}", id)
         .retrieve()
+        .onStatus(HttpStatus::is4xxClientError, response -> Mono.empty())
         .bodyToFlux(Review.class)
         .collectList()
         .log()
+        .retry(5)
+//        .retryWhen(Retry.fixedDelay(5, Duration.ofSeconds(1)))
+        .timeout(Duration.ofMillis(200)) // 모든 retry 합한 시간에 대한 timeout
         .onErrorReturn(Collections.emptyList());
   }
 }
