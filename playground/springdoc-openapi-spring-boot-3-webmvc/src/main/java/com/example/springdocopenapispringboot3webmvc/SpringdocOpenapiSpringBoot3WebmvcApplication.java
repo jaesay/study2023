d36630a7d828aeab8c44830ccbdc76700ee7d147
Kim Jae Seong <jaesay@ajou.ac.kr>
2023-07-06 21:15:06 +0900

    springdoc-openapi-spring-boot-3-webmvc 프로젝트 생성

---
+++ playground/springdoc-openapi-spring-boot-3-webmvc/src/main/java/com/example/springdocopenapispringboot3webmvc/SpringdocOpenapiSpringBoot3WebmvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdocopenapispringboot3webmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdocOpenapiSpringBoot3WebmvcApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringdocOpenapiSpringBoot3WebmvcApplication.class, args);
+  }
+
+}
---
+++ playground/springdoc-openapi-spring-boot-3-webmvc/src/test/java/com/example/springdocopenapispringboot3webmvc/SpringdocOpenapiSpringBoot3WebmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdocopenapispringboot3webmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdocOpenapiSpringBoot3WebmvcApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
