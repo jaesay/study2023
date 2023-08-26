4fdf79d00f014460f879ce50a2e863fa6ce9efda
Kim Jae Seong <jaesay@ajou.ac.kr>
2023-08-26 15:46:52 +0900

    Chapter 6 예제 추가

--- spring-reactive/src/test/java/com/example/springreactive/reactor/Example6.java
+++ spring-reactive/src/test/java/com/example/springreactive/reactor/Example6.java
@@ -4,6 +4,7 @@ import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 import java.net.URI;
 import java.util.Collections;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpEntity;
@@ -12,6 +13,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class Example6 {
@@ -69,5 +71,46 @@ public class Example6 {
             System.out::println,
             () -> System.out.println("# emitted onComplete signal")
         );
+  }
+
+  @Test
+  @DisplayName("Flux 기본 예제")
+  void example6_4() {
+    Flux.just(3, 6, 13)
+        .map(num -> num % 2)
+        .subscribe(System.out::println);
+  }
+
+  @Test
+  @DisplayName("Flux 에서의 Operator 체인 사용 예제")
+  void example6_5() {
+    Flux.fromArray(new Integer[]{3, 6, 7, 9})
+        .filter(num -> num > 6)
+        .map(num -> num * 2)
+        .subscribe(System.out::println);
+  }
+
+  @Test
+  @DisplayName("2개의 Mono를 연결해서 Flux로 변환하는 예제")
+  void example6_6() {
+    Flux<String> flux = Mono
+        .justOrEmpty("Steve") // just() Operator의 경우 파라미터의 값으로 null을 허용하지 않지만 justOrEmpty()는 null을 허용한다. null이 전달되면 내부적으로 empty() Operator를 호출하도록 구현되어 있다.
+        .concatWith(Mono.justOrEmpty("Jobs")); // concatWith() Operator는 concatWith()를 호출하는 Publisher와 concatWith()의 파라미터로 전달되는 Publisher가 각각 emit하는 데이터들을 하나로 연결해서 새로운 Publisher의 데이터 소스로 만들어 준다.
+
+    flux.subscribe(System.out::println);
+  }
+
+  @Test
+  @DisplayName("여러개의 Flux를 연결해서 하나의 Flux로 결합하는 예제")
+  void example6_7() {
+    // concatWith()의 경우 두 개의 데이터 소스만 연결할 수 있지만, concat()은 여러 개의 데이터 소스를 원하는 만큼 연결할 수 있다.
+    Mono<List<String>> listMono = Flux.concat(
+            Flux.just("Mercury", "Venus", "Earth"),
+            Flux.just("Mars", "Jupiter", "Saturn"),
+            Flux.just("Uranus", "Neptune", "Pluto"))
+        .collectList(); // collectList() Operator는 Upstream Publisher에서 emit하는 데이터를 모아서 List의 원소로 포함시킨 새로운 데이터 소스를 만들어 준다.
+
+    listMono
+        .subscribe(System.out::println);
   }
 }
