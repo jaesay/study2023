7f80a38650bd90538244b0f5df2a7bf6c1dcc819
Kim Jae Seong <jaesay@ajou.ac.kr>
2023-09-13 22:51:19 +0900

    webclient-resilience4j 프로젝트 생성

---
+++ playground/webclient-resilience4j/src/test/java/com/example/webclientresilience4j/WebclientResilience4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webclientresilience4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebclientResilience4jApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
