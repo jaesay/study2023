6598761222694d6183276fdce59bcce70794a1a8
Kim Jae Seong <jaesay@ajou.ac.kr>
2023-09-14 21:49:19 +0900

    hexarch 프로젝트 생성

---
+++ playground/hexarch/src/test/java/com/example/hexarch/HexarchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hexarch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexarchApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
