a25615cde1ac72ef815abe7b3e6bb71f159a57a7
Kim Jae Seong <jaesay@ajou.ac.kr>
2023-09-22 23:32:33 +0900

    webflux-cacheable 프로젝트 생성

---
+++ playground/webflux-cacheable/src/main/java/com/example/webfluxcacheable/WebfluxCacheableApplication.java
@@ -0,0 +1,13 @@
+package com.example.webfluxcacheable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxCacheableApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WebfluxCacheableApplication.class, args);
+  }
+
+}
---
+++ playground/webflux-cacheable/src/test/java/com/example/webfluxcacheable/WebfluxCacheableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webfluxcacheable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxCacheableApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
