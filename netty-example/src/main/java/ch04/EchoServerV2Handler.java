ea3acf881f3728b6698384c610725ccb7b2e335d
Kim Jae Seong <jaesay@ajou.ac.kr>
2023-09-29 23:14:31 +0900

    이벤트 핸들러 예제 추가

---
+++ netty-example/src/main/java/ch04/EchoServerV2Handler.java
@@ -0,0 +1,28 @@
+package ch04;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import java.nio.charset.Charset;
+
+public class EchoServerV2Handler extends ChannelInboundHandlerAdapter {
+
+  @Override
+  public void channelRead(ChannelHandlerContext ctx, Object msg) {
+    ByteBuf readMessage = (ByteBuf) msg;
+    System.out.println("channelRead : " + readMessage.toString(Charset.defaultCharset()));
+    ctx.write(msg);
+  }
+
+  @Override
+  public void channelReadComplete(ChannelHandlerContext ctx) {
+    System.out.println("channelReadComplete 발생");
+    ctx.flush();
+  }
+
+  @Override
+  public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+    cause.printStackTrace();
+    ctx.close();
+  }
+}
