0b6b9736a6437646c9ca9b712db7397dc2df74ec
Kim Jae Seong <jaesay@ajou.ac.kr>
2023-09-30 13:38:47 +0900

    비동기 I/O 처리 예체 추가

---
+++ netty-example/src/main/java/ch05/EchoServerHandler.java
@@ -0,0 +1,23 @@
+package ch05;
+
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+@Sharable
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+  @Override
+  public void channelRead(ChannelHandlerContext ctx, Object msg) {
+    ChannelFuture channelFuture = ctx.writeAndFlush(msg);
+    channelFuture.addListener(ChannelFutureListener.CLOSE);
+  }
+
+  @Override
+  public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+    cause.printStackTrace();
+    ctx.close();
+  }
+}
