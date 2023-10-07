59fba81ff1ad66a6a63f1109adf2b706ea31d50a
Kim Jae Seong <jaesay@ajou.ac.kr>
2023-10-07 17:45:34 +0900

    어댑터 어노테이션 추가

---
+++ playground/hexarch/src/main/java/com/example/hexarch/common/RestAdapter.java
@@ -0,0 +1,19 @@
+package com.example.hexarch.common;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.web.bind.annotation.RestController;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@RestController
+public @interface RestAdapter {
+
+  @AliasFor(annotation = RestController.class)
+  String value() default "";
+}
