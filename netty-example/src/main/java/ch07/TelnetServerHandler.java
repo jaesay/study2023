d066b38da83351b6ed08e964ac1cf76cf801f686
Kim Jae Seong <jaesay@ajou.ac.kr>
2024-01-01 18:02:32 +0900

    주석 추가

--- netty-example/src/main/java/ch07/TelnetServerHandler.java
+++ netty-example/src/main/java/ch07/TelnetServerHandler.java
@@ -9,8 +9,14 @@ import io.netty.channel.SimpleChannelInboundHandler;
 import java.net.InetAddress;
 import java.util.Date;
 
-@Sharable
-public class TelnetServerHandler extends SimpleChannelInboundHandler<String> {
+@Sharable // 1. Sharable 어노테이션은 네티가 제공하는 공유가능 상태표시 어노테이션이다. Sharable로 지정된 클래스를 채널 파이프라인에서 공유할 수 있다는 의미이다. 즉, 다중 스레드에서 스레드 경합 없이 참조가 가능하다.
+public class TelnetServerHandler extends SimpleChannelInboundHandler<String> { // 여기에 지정된 제네릭 타입은 데이터 수신 이벤트인 channelRead0 메서드의 두 번째 인수의 데이터형이 된다. TelnetServerHandler 클래스에서는 수신된 데이터가 String 데이터임을 의미한다.
+
+  /**
+   * channelActive 메서드는 채널이 생성된 다음 호출되는 이벤트다.
+   * 서버 프로그램을 예로 들면 클라이언트가 서버에 접속되면 네티의 채널이 생성되고 해당 채널이 호출된다.
+   * 통상적으로 채널이 연결된 직후에 수행할 작업을 처리할 때 사용하는 이벤트다.
+   */
   @Override
   public void channelActive(ChannelHandlerContext ctx) throws Exception {
     ctx.write(InetAddress.getLocalHost().getHostName() + " 서버에 접속 하셨습니다!\r\n");
@@ -38,12 +44,16 @@ public class TelnetServerHandler extends SimpleChannelInboundHandler<String> {
     // ChannelBuffer에 전송할 값을 복사하여 전송하지 않고 직접 문자열을 전송한다.
     ChannelFuture future = ctx.write(response);
 
-    // 클라이언트로부터 bye 명령을 수신하면 close 플래그가 true값이 되므로 연결을 끊는다.
+    // 종료 명령 플래그를 확인하여 연결된 클라이언트의 채널을 닫는다. 이때 ChannelFuture에 ChannerFutureListener.CLOSE를 등록하여 비동기로 채널을 닫는다.
     if (close) {
       future.addListener(ChannelFutureListener.CLOSE);
     }
   }
 
+  /**
+   * channelReadComplete 이벤트는 channelRead0 이벤트가 완료되면 호추ㅜㄹ되는 이벤트이다.
+   * 여기서 ChannelHandlerContext의 flush 메서드를 사용하여 채널에 기록된 데이터를 클라이언트로 즉시 전송한다.
+   */
   @Override
   public void channelReadComplete(ChannelHandlerContext ctx) {
     ctx.flush();
--- netty-example/src/main/java/ch07/TelnetServerInitializer.java
+++ netty-example/src/main/java/ch07/TelnetServerInitializer.java
@@ -15,6 +15,7 @@ import io.netty.util.CharsetUtil;
  * Creates a newly configured {@link ChannelPipeline} for a new channel.
  */
 public class TelnetServerInitializer extends ChannelInitializer<SocketChannel> {
+
   private static final StringDecoder DECODER = new StringDecoder(CharsetUtil.UTF_8);
   private static final StringEncoder ENCODER = new StringEncoder(CharsetUtil.UTF_8);
 
@@ -26,11 +27,11 @@ public class TelnetServerInitializer extends ChannelInitializer<SocketChannel> {
 
     // Add the text line codec combination first,
     pipeline.addLast(new DelimiterBasedFrameDecoder(8192, Delimiters.lineDelimiter()));
-    // the encoder and decoder are static as these are sharable
+    // 새로운 클라이언트 채널들이 동일한 인코더/디코더 객체를 공유하게 된다.
     pipeline.addLast(DECODER);
     pipeline.addLast(ENCODER);
 
-    // and then business logic.
+    // 위 인코더/디코더와 마찬가지로 모든 채널 파이프라인에서 공유된다.
     pipeline.addLast(SERVER_HANDLER);
   }
 }
