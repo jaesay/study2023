83e578bc7096ce5cfef3201edb52d32e97d66267
Kim Jae Seong <jaesay@ajou.ac.kr>
2024-01-10 20:40:23 +0900

    Spring Data JPA 적용

--- db2/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ db2/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,6 +2,7 @@ package hello.itemservice;
 
 import hello.itemservice.config.JpaConfig;
 import hello.itemservice.config.MyBatisConfig;
+import hello.itemservice.config.SpringDataJpaConfig;
 import hello.itemservice.repository.ItemRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,7 +15,8 @@ import org.springframework.context.annotation.Profile;
 //@Import(JdbcTemplateV2Config.class)
 //@Import(JdbcTemplateV3Config.class)
 //@Import(MyBatisConfig.class)
-@Import(JpaConfig.class)
+//@Import(JpaConfig.class)
+@Import(SpringDataJpaConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
