33e2ed155ac35754ecf2c2a6f86bd77afbc746ed
Kim Jae Seong <jaesay@ajou.ac.kr>
2024-01-15 22:49:31 +0900

    BasicTxTest 수정
    - 스프링 트랜잭션 전파 옵션인 REQUIRES_NEW 동작 확인

--- db2/springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ db2/springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.UnexpectedRollbackException;
 import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
@@ -122,6 +123,29 @@ class BasicTxTest {
     log.info("외부 트랜잭션 커밋");
     assertThatThrownBy(() -> txManager.commit(outer))
         .isInstanceOf(UnexpectedRollbackException.class);
+  }
+
+  @Test
+  @DisplayName("기존 트랜잭션에 참여하는 것이 아니라 새로운 물리 트랜잭 션을 만들어서 시작하게 된다.")
+  void inner_rollback_requires_new() {
+    log.info("외부 트랜잭션 시작");
+    TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+    log.info("outer.isNewTransaction()={}", outer.isNewTransaction());
+
+    log.info("내부 트랜잭션 시작");
+    DefaultTransactionAttribute definition = new DefaultTransactionAttribute();
+    definition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+    // Suspending current transaction, creating new transaction with name [null]
+    // Acquired Connection
+    TransactionStatus inner = txManager.getTransaction(definition);
+    log.info("inner.isNewTransaction()={}", inner.isNewTransaction());
+    log.info("내부 트랜잭션 롤백");
+    // Releasing JDBC Connection
+    // Resuming suspended transaction after completion of inner transaction
+    txManager.rollback(inner); //롤백
+
+    log.info("외부 트랜잭션 커밋");
+    txManager.commit(outer); //커밋
   }
 
 }
